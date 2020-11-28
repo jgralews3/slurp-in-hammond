3e91768e7048a45529aff6d6421890feda1622e7
Joshua Gralewski <jgralews3@yahoo.com>
2020-11-28 10:26:59 -0600

    start

--- src/App.js
+++ src/App.js
@@ -1,56 +1,28 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
+    <div>
+    <nav>
+        <a>Menu</a>
+        <a>Contact Us</a>
+        <a>Why Yesenia Should Trust Her Friend More</a>
+      </nav>
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Slurp!</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
+        Hammond's Finest (and Most Exclusive) Coffee Boutique
       </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <div className="content">
+        <p className="content-text">
+    Welcome to the hottest (but not too hot) coffee establishment in Hammond. Never heard of it? That's because it's invite only. Located in a private location, it's located in where you wouldn't think: a location.
+        </p>
+        <img href="https://images.pexels.com/photos/1405761/pexels-photo-1405761.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="coffee pic" />
+      </div>
+
     </main>
+    </div>
   );
 }
 
